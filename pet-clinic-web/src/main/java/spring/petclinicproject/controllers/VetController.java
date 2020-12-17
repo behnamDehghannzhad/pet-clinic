8ce8639c5139f88d70b3d1b6c96a216dd0b4f98c
behnamDehghannzhad <b.dehghannezhad.gitHub@gmail.com>
2020-12-17 23:41:47 +0330

    adding list of vet and update controller

--- pet-clinic-web/src/main/java/spring/petclinicproject/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring/petclinicproject/controllers/VetController.java
@@ -1,13 +1,27 @@
 package spring.petclinicproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.petclinicproject.model.Vet;
+import spring.petclinicproject.services.map.VetServiceMap;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String vetList(){
+    private VetServiceMap vetServiceMap;
+
+    public VetController(VetServiceMap vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String vetList(Model model) {
+
+        Set<Vet> vets = vetServiceMap.findAll();
+        model.addAttribute("allVets", vets);
 
         return "vets/index";
     }
